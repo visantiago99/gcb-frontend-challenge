809bc28409ab7d5bd4463647691ad011973ce6f9
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-12-01 18:29:52 -0300

    Add register formulary and LocalStorage

--- src/components/Formulary.jsx
+++ src/components/Formulary.jsx
@@ -1,25 +1,31 @@
 import React, { useState, useEffect } from 'react';
-
+import axios from 'axios';
 
 export default function Formulary() {
   const [formulary, setFormulary] = useState({
-    nome: '',
-    nascimento: '',
+    name: '',
+    birthdate: '',
     cpf: '',
     cep: '',
-    // bairro: '',
-    // cidade: '',
-    // estado: '',
-    // logradouro: ''
   })
-  // const [cepData, setCepData] = useState('');
+  const [cepData, setCepData] = useState('');
 
+  const fetchCEP = async (cep) => {
+    try {
+      if (formulary.cep.length === 8) {
+        const fetchData = await axios.get(`https://viacep.com.br/ws/77020468/json/`)
+        console.log(fetchData)
+        return setCepData(fetchData.data)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchCEP()
+  }, [formulary.cep])
   
-  // const fetchCEP = async (cep) => {
-  //   const fetchData = await fetch(`http://correiosapi.apphb.com/cep/${cep}`)
-  //   const res = fetchData.json()
-  //   return setCepData(res)
-  // }
 
   const handleChange = ({target}) => {
     setFormulary({
@@ -28,42 +34,81 @@ export default function Formulary() {
     })
   }
 
+  const setLocalStorage = () => {
+    const userObj = {
+      name: formulary.name,
+      birthdate: formulary.birthdate,
+      cpf: formulary.cpf,
+      cep: formulary.cep,
+      neighborhood: cepData.bairro,
+      city: cepData.localidade,
+      state: cepData.uf,
+      street: cepData.logradouro,
+    }
+    if (!localStorage.userList) {
+      localStorage.setItem('userList', JSON.stringify([userObj]))
+      return 
+    }
+    const localObj = JSON.parse(localStorage.getItem('userList'))
+    let result = [...localObj, userObj]
+    localStorage.setItem('userList', JSON.stringify(result))
+  }
+
+  const clearForm = () => {
+    setFormulary({
+      name: '',
+      birthdate: '',
+      cpf: '',
+      cep: '',
+    })
+    setCepData({
+      bairro: '',
+      localidade: '',
+      uf: '',
+      logradouro: '',
+    })
+  }
+
   return (
     <div className="formulary-container">
       <form>
         <label>
-          Nome completo
-          <input type="text" name="nome" onChange={(e) => handleChange(e)} />
+          Fullname
+          <input type="text" name="name" onChange={(e) => handleChange(e)} value={formulary.name} />
         </label>
         <label>
-          Data de nascimento
-          <input type="date" name="nascimento" onChange={(e) => handleChange(e)} />
+          Birthdate
+          <input type="date" name="birthdate" onChange={(e) => handleChange(e)} value={formulary.birthdate} />
         </label>
         <label>
           CPF
-          <input type="number" name="cpf" onChange={(e) => handleChange(e)} />
+          <input type="number" name="cpf" onChange={(e) => handleChange(e)} value={formulary.cpf} />
         </label>
         <label>
           CEP
-          <input type="number" name="cep" onChange={(e) => handleChange(e)} />
+          <input type="number" name="cep" onChange={(e) => handleChange(e)} maxLength='8' value={formulary.cep} />
         </label>
         <label>
-        bairro
-          <input type="text" name="bairro" onChange={(e) => handleChange(e)} />
+        Neighborhood
+          <input type="text" name="Neighborhood" value={cepData.bairro}  />
         </label>
         <label>
-        cidade
-          <input type="text" name="cidade" onChange={(e) => handleChange(e)} />
+        City
+          <input type="text" name="City" value={cepData.localidade} />
         </label>
         <label>
-        estado
-          <input type="text" name="estado" onChange={(e) => handleChange(e)} />
+        State
+          <input type="text" name="State" value={cepData.uf} />
         </label>
         <label>
-        logradouro
-          <input type="text" name="logradouro" onChange={(e) => handleChange(e)} />
+        Street
+          <input type="text" name="Street" value={cepData.logradouro} />
         </label>
       </form>
+      <button type="button" onClick={() => {
+        setLocalStorage()
+        clearForm()
+      }}>Register</button>
     </div>
   )
 }
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Formulary from '../components/Formulary';
+import Footer from '../components/Footer';
 
 export default function RegisterPage() {
   return (
     <div>
       <Header />
       <Formulary />
+      <Footer />
     </div>
   )
 }
